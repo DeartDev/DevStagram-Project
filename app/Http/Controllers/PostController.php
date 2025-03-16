48cb23027b9061d5bc0427c0732445562852dae7
Decar08 <deart.94@gmail.com>
2025-03-16 17:08:37 -0500

    Se hace uso de la caracteristica Route Model Binding, para asociar en la url el usuario de la persona autenticado

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     // Redireccion al muro
-    public function index()
+    public function index(User $user)
     {
+        // dd($user->username);
         return view('dashboard');
 
     }
