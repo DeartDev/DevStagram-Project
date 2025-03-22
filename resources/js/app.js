ee39a0b39224e094527e40ea13d6bb6653e6a6ca
Decar08 <deart.94@gmail.com>
2025-03-22 12:01:24 -0500

    Se agregan eventos de dropzone dependiendo de lo que ocurra con la carga de los archivos

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,4 +10,26 @@ const dropzone = new Dropzone('#dropzone', {
     maxFiles: 1,
     uploadMultiple: false,
 });
-    
+
+
+// Eventos en DropZone
+// Evento para cuando se está subiendo un archivo
+dropzone.on('sending', function(file, xhr, formData) {
+    console.log('Enviando archivo');
+});
+
+ 
+// Evento para cuando se ha subido un archivo
+dropzone.on('success', function(file, response) {
+    console.log(response);
+});
+
+// Evento para cuando ha ocurrido un error al subir un archivo
+dropzone.on('error', function(file, message) {
+    console.log(message);
+});
+
+// Evento para cuando se ha eliminado un archivo
+dropzone.on('removedfile', function() {
+    console.log('Archivo eliminado');
+});
