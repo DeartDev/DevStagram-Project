5f196beb968deb007f1d162d729fb736eb25fb7b
Decar08 <deart.94@gmail.com>
2025-04-10 22:12:48 -0500

    102-ok -> Se ejecuta el factory para la creacion de registros de prueba en la base de datos local

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -21,7 +21,7 @@ class PostFactory extends Factory
             'titulo' => $this->faker->sentence(5),
             'descripcion' => $this->faker->sentence(20),
             'imagen' => $this->faker->uuid() . '.jpg',
-            'user_id' => $this->faker->randomElement(['1', '2']),
+            'user_id' => $this->faker->randomElement(['1', '2' ]),
         ];
     }
 }
