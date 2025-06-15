adfeb3bc8a8d8b98b10a13b02e368f872bcffe92
DeartDev <deart.94@gmail.com>
2025-06-15 16:15:51 -0500

    114-ok, se agrega condicional en la vista de dashboard para validar si existen post en el perfil, si existen los mostrara, de lo contrario mostrara un mensaje que diga que aun no existen posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -40,19 +40,25 @@
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-bold my-10">Publicaciones</h2>
 
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            
-            @foreach ($posts as $post)
+        @if($posts->count())
 
-                <div>
-                    <a href="">
-                        <img src="{{ asset('uploads') .'/' . $post->imagen }}" alt="imagen del post {{ $post->titulo }}">
-                    </a>
-                </div>
-                
-            @endforeach
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
 
-        </div>
+                @foreach ($posts as $post)
+
+                    <div>
+                        <a href="">
+                            <img src="{{ asset('uploads') .'/' . $post->imagen }}" alt="imagen del post {{ $post->titulo }}">
+                        </a>
+                    </div>
+                    
+                @endforeach
+
+            </div>
+
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay publicaciones aún</p>
+        @endif
         
 
     </section>
