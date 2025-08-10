b96b6569a2dc0273d7713006bcfc93e67e04ea9a
DeartDev <deart.94@gmail.com>
2025-08-10 14:45:41 -0500

    121-ok se agrega cuadro de comentarios

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,8 +25,24 @@
             
         </div>
 
-        <div class="md:w-1/2">
-            2
+        <div class="md:w-1/2 p-5">
+            <div claass="shadow bg-white p-5 mb-5">
+                <p class="text-xl font-bold text-center mb-4">Agrega un comentario</p>
+
+                <form action="">
+                        <div class="mb-5">
+                        <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">Comentario</label>
+                        <textarea id="comentario" name="comentario" placeholder="Deja un comentario" class="border p-3 w-full rounded-lg @error ('comentario') border-red-500 @enderror"></textarea>
+
+                        @error('comentario')
+                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                        @enderror
+                    </div>
+
+                    <input type="submit" name="comentar" id="comentar" value="comentar" class="bg-sky-600 hover:bg-sky-800 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+
+                </form>
+            </div>
         </div>
 
     </div>
